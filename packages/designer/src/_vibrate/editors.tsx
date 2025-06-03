08c54aef5164423891028d4a8f06cdd61e4efaf4
moyukai05 <moyukai05@kuaishou.com>
2025-06-03 15:07:00 +0800

    feat:update

---
+++ packages/designer/src/_vibrate/editors.tsx
@@ -0,0 +1,16 @@
+export default {
+  ":root": [
+    {
+      // title: '是否只允许从相机扫码',
+      // type: 'Switch',
+      // value: {
+      //   get({ data }) {
+      //     return data.onlyFromCamera;
+      //   },
+      //   set({ data }, value: boolean) {
+      //     data.onlyFromCamera = value;
+      //   }
+      // }
+    },
+  ],
+};
