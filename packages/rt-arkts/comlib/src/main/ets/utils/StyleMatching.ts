7ceda00e2ebe3d96c4c4e7f1384aa44ae55a5896
moyukai05 <moyukai05@kuaishou.com>
2025-07-03 11:43:04 +0800

    feat:update

--- packages/rt-arkts/comlib/src/main/ets/utils/StyleMatching.ts
+++ packages/rt-arkts/comlib/src/main/ets/utils/StyleMatching.ts
@@ -3,8 +3,8 @@ import { AnyType } from '../types'
 export  function getStyleValueByPattern<T>(
   styles: Record<string, AnyType>,
   selectorParts: string[],
-  property: string,
-  defaultValue: T
+  property?: string,
+  defaultValue: T = undefined
 ): T {
   // 处理输入的选择器：拆分、排序、重组
   const normalizeSelector = (selector: string) => {
@@ -20,7 +20,7 @@ export  function getStyleValueByPattern<T>(
       // 移除'px'并转换为数字
       return parseInt(value.replace('px', ''));
     }
-    return value;
+    return value || 0;
   };
 
   // 规范化目标选择器
@@ -30,6 +30,41 @@ export  function getStyleValueByPattern<T>(
   const matchingKey = Object.keys(styles).find(key =>
   normalizeSelector(key) === targetSelector
   );
+  if(property){
+    return matchingKey ? processValue(styles[matchingKey][property]) : defaultValue;
+  }else{
+    return matchingKey ? styles[matchingKey] : defaultValue;
+  }
+}
 
-  return matchingKey ? processValue(styles[matchingKey][property]) : defaultValue;
+export function parseRadius(radiusStr:string):AnyType {
+  const values = radiusStr.trim().split(/\s+/);
+  const numbers = values.map(val => parseInt(val));
+  switch (numbers.length) {
+    case 1:
+      return numbers[0];
+    case 2:
+      return {
+        topLeft: numbers[0],
+        topRight: numbers[1],
+        bottomRight: numbers[0],
+        bottomLeft: numbers[1]
+      };
+    case 3:
+      return {
+        topLeft: numbers[0],
+        topRight: numbers[1],
+        bottomRight: numbers[2],
+        bottomLeft: numbers[1]
+      };
+    case 4:
+      return {
+        topLeft: numbers[0],
+        topRight: numbers[1],
+        bottomRight: numbers[2],
+        bottomLeft: numbers[3]
+      };
+    default:
+      return 0;
+  }
 }
