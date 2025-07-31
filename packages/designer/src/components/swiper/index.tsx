fd76ea9e734b24d2e0ae1c1ba28c411f3f14d4bc
liubiao03 <liubiao03@kuaishou.com>
2025-07-31 12:10:06 +0800

    feat: 编辑态不使用swiper

--- packages/designer/src/components/swiper/index.tsx
+++ packages/designer/src/components/swiper/index.tsx
@@ -1,39 +1,60 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import {
   View,
-  Swiper as _Swiper,
+  Swiper as TaroSwiper,
   SwiperItem,
   SwiperProps,
-} from "@tarojs/components";
-import Taro from "@tarojs/taro";
+} from '@tarojs/components'
 import { polyfill_taro_swiper } from './../../utils/h5-polyfill'
-import css from "./index.less";
+import css from './index.less'
 
-polyfill_taro_swiper();
+polyfill_taro_swiper()
 
 function findArrayOrObject(arr) {
   for (let item of arr) {
     if (Array.isArray(item)) {
-      return item;
+      return item
     } else if (typeof item === 'object' && item !== null) {
       return [item]
     }
   }
-  return null; // 如果没有找到数组或对象，返回 null
+  return null // 如果没有找到数组或对象，返回 null
 }
 
-export function Swiper(props: SwiperProps) {
-  const { env, data, current, style, children, className, indicator, ...extra } = props;
-  const [swiperKey,setSwiperKey] = useState(0)
+interface CustomSwiperProps extends SwiperProps {
+  env: Env
+  data: Data
+  indicator: boolean
+}
+
+export function Swiper(props: CustomSwiperProps) {
+  const {
+    env,
+    data,
+    current,
+    style,
+    children,
+    className,
+    indicator,
+    ...extra
+  } = props
+  const [swiperKey, setSwiperKey] = useState(0)
 
   //判断是否是真机运行态
   const isRelEnv = useMemo(() => {
     if (env.runtime.debug || env.edit) {
-      return false;
+      return false
     } else {
-      return true;
+      return true
     }
-  }, [env?.runtime?.debug,env.edit])
+  }, [env?.runtime?.debug, env.edit])
+
+  const SwiperComp = useMemo(() => {
+    if (env.edit) {
+      return View
+    }
+    return TaroSwiper
+  }, [env.edit])
 
   // useEffect(()=>{
   //   const randomNumber = Math.floor(Math.random() * 1000000000);
@@ -42,7 +63,7 @@ export function Swiper(props: SwiperProps) {
 
   return (
     <View className={`${css.wrapper} mybricks-swiper-wrapper ${className}`}>
-      <_Swiper
+      <SwiperComp
         key={swiperKey}
         {...extra}
         // className={`${css.swiper} mybricks-swiper`}
@@ -51,23 +72,27 @@ export function Swiper(props: SwiperProps) {
         indicatorDots={false}
       >
         {children}
-      </_Swiper>
+      </SwiperComp>
       {indicator && (
-        <View className={"indicators"}>
-          {Array.from(isRelEnv ? findArrayOrObject(children) : data.items).map((raw, index) => {
-            return (
-              <View
-                key={index}
-                className={
-                  current === index ? "indicator indicator-active" : "indicator"
-                }
-              ></View>
-            );
-          })}
+        <View className={'indicators'}>
+          {Array.from(isRelEnv ? findArrayOrObject(children) : data.items).map(
+            (raw, index) => {
+              return (
+                <View
+                  key={index}
+                  className={
+                    current === index
+                      ? 'indicator indicator-active'
+                      : 'indicator'
+                  }
+                ></View>
+              )
+            }
+          )}
         </View>
       )}
     </View>
-  );
+  )
 }
 
-export { SwiperItem };
+export { SwiperItem }
