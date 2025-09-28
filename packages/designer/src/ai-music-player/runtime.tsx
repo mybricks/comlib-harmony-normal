3d51693a533bfa21ff84728aa181e2393d4b0fe4
liubiao03 <liubiao03@kuaishou.com>
2025-09-28 10:55:26 +0800

    feat: music player

---
+++ packages/designer/src/ai-music-player/runtime.tsx
@@ -0,0 +1,41 @@
+import React, { useCallback, useMemo } from "react";
+import { View } from "@tarojs/components";
+import css from "./style.less";
+import cx from "classnames";
+
+export default function ({ env, data, slots, inputs, outputs }) {
+  const onEditClick = useCallback((e) => {
+    if (!env.runtime) {
+      return;
+    }
+    // 当配置了单击事件，阻止事件冒泡
+    if (outputs["onEditClick"].getConnections().length) {
+      e.stopPropagation();
+    }
+    outputs["onEditClick"]?.();
+  }, []);
+
+  const onPlayListClick = useCallback((e) => {
+    if (!env.runtime) {
+      return;
+    }
+    // 当配置了单击事件，阻止事件冒泡
+    if (outputs["onPlayListClick"].getConnections().length) {
+      e.stopPropagation();
+    }
+    outputs["onPlayListClick"]?.();
+  }, []);
+
+  return (
+      <View className={cx(css.player)} onClick={onEditClick}>
+        <div className={cx(css.content)}>
+          <div className={cx(css.name)}>歌曲名称</div>
+          <div className={cx(css.toolbar)}>
+            {slots["toolbar"].render()}
+          </div>
+        </div>
+
+        <div className={cx(css.playlist)} onClick={onPlayListClick}>播放列表</div>
+      </View>
+  );
+}
