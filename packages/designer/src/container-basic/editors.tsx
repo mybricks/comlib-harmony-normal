32871125a06fe829a9457ae46e6f4b41ec32d50d
moyukai05 <moyukai05@kuaishou.com>
2025-09-29 11:22:31 +0800

    fix:容器组件当滚动时事件兼容

--- packages/designer/src/container-basic/editors.tsx
+++ packages/designer/src/container-basic/editors.tsx
@@ -18,6 +18,18 @@ export default {
       },
     ],
     items: ({ data, output, style }, cate0, cate1, cate2) => {
+      //兼容老版本容器的「当容器滚动」事件
+      output.add({
+        id: "onScroll",
+        title: "当容器滚动时",
+        schema: {
+          type: "object",
+          properties: {
+          },
+        },
+        editable: true,
+        deletable: true,
+      });
       cate0.title = "常规";
       cate0.items = [
         {
