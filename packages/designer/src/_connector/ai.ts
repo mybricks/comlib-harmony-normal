ef24ca8608ea4bba53e4ebefc140847c9a9a0592
leon-js <42924541+leon-js@users.noreply.github.com>
2025-12-22 14:53:18 +0800

    update

--- packages/designer/src/_connector/ai.ts
+++ packages/designer/src/_connector/ai.ts
@@ -6,11 +6,11 @@ export default {
     summary: "服务接口，发起网络请求的必要组件",
     usage: `
 输入端口：
-  - call 调用
+  - call 调用接口
     
 输出端口：
-  - then 结果
-  - catch 发生错误
+  - then 结果，接口正常返回内容
+  - catch 发生错误，接口调用过程中报错，异常返回内容
 `,
   },
 };
