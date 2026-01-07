51f59878f0c8e1f233ed469a451a5fdbd1d141b7
liubiao03 <liubiao03@kuaishou.com>
2026-01-07 11:55:09 +0800

    feat: update

--- packages/designer/src/container-basic/ai.ts
+++ packages/designer/src/container-basic/ai.ts
@@ -6,19 +6,24 @@ export default {
   prompts: {
     summary: '基础布局组件，可以用做布局组件和背景样式容器，必须使用',
     usage: `基础布局组件，可以用做布局组件和背景样式容器，必须使用。
-
 slots插槽
 content 内容
 
 layout声明
-width: 可配置，默认100%
-height: 可配置，默认160
+width: 可配置，默认100%；
+height: 可配置，当display=flex时，可以配置fit-content，其余为固定数值；
 
-<配置流程>
-  1. 确认配置布局编辑器为flex布局；
-  2. 由于宽度、高度都和布局相关，需要根据确认的布局，完成宽高的配置；
-  3. 根据需求完成其它layout和样式配置；
-</配置流程>
+配置步骤
+- 确认布局：确认当前布局信息，必须配置；
+- 根据布局完成宽高配置：
+  - 当声明display=flex时，layout属性宽高需遵循下方类型定义配置:
+    width: number(固定px) | '100%' ｜ 'fit-content' | 'auto'
+    height: number(固定px) | 'fit-content'
+  - 当声明position=smart时，layout属性宽高需遵循下方类型定义配置:
+    width: number(固定px) | '100%' | 'auto'
+    height: number(固定px)
+- 根据需求完成其它layout和样式配置；
 `
-  }
+  },
+  asLayout: true
 }
